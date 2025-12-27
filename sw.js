bd44d0ff8680340de0107f01856d623debf0b397
Manik Roy <pixnum@gmail.com>
2025-12-27 12:07:01 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,22 @@
-const cacheName = 'nameyer-cache-v1';
-const assets = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png',
-  // Add other CSS/JS files if separate
+const CACHE_NAME = "nameyer-v1";
+const BASE_PATH = "/Nameyer/";
+
+const ASSETS = [
+  BASE_PATH,
+  BASE_PATH + "index.html",
+  BASE_PATH + "nameyer-manifest.json",
+  BASE_PATH + "icon-192.png",
+  BASE_PATH + "icon-512.png"
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(assets))
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
